139bd22fd852232beabb0efc124eedaab82137cd
Lukas B <github@lbergmann.de>
2021-07-08 14:18:08 +0200

    Major Update
    +fixed various odr problems
    +added dynamic but fast rising iteration count per parameter
    +added counting x / y data for empty data list (..._column == -1)
    +added simple "add" error type which simply adds column to respective error
    +fixed formatting error
    +[COMPATIBILITY WARNING] renamed shift_x/y to stretch_x/y
    +added offset_x/y
    +added accessible fit params and uncerts
    +fixed rounding issues for reduce
    -removed y rotation
    +added custom param_names and debug flag to fit_plot
    +fixed legends_err so that it doesn't need to be full length

--- EzToPlot.py
+++ EzToPlot.py
@@ -8,15 +8,17 @@ from decimal import *
 
 getcontext().prec = 20
 
-def perform_odr(model, parameter_count, x, y, xerr, yerr, override_start=[]):
+#thanks to https://stackoverflow.com/a/3413529
+def round_sig(x, sig=3):
+	return round(x, sig-int(np.floor(np.log10(abs(x))))-1)
+
+def perform_odr(model, parameter_count, x, y, xerr, yerr, override_start=[], debug=False):
     odr_model = odr.Model(model)
     if(0 in xerr):
-        odr_data = odr.RealData(x, y, sy=yerr)
+        xerr = np.multiply(x,10**(-10))
     elif(0 in yerr):
-        odr_data = odr.RealData(x, y, sx=xerr)
-    else:
-        odr_data = odr.RealData(x, y, sx=xerr, sy=yerr)
-
+        yerr = np.multiply(y,10**(-10))
+    odr_data = odr.RealData(x, y, sx=xerr, sy=yerr)
     beta0 = []
 
     if(len(override_start)<=0):#TODO: fix so we can pass one array
@@ -25,8 +27,10 @@ def perform_odr(model, parameter_count, x, y, xerr, yerr, override_start=[]):
     else:
         beta0 = override_start
 
-    odr_regression = odr.ODR(odr_data, odr_model, beta0=beta0, maxit=50)
+    odr_regression = odr.ODR(odr_data, odr_model, beta0=beta0, maxit=50**parameter_count)
     output = odr_regression.run()
+    if debug == True:
+        output.pprint()
     return output
 
 def get_error(value_column, plain_values,err_types,err_settings,err_columns):
@@ -60,13 +64,15 @@ class file_parser:
         self.y_data = []
         self.x_err = []
         self.y_err = []
+        self.fit_params = []
+        self.fit_params_uncert = []
         self.check_cohearance()
         self.parse_file()
 
     def parse_file(self):
         plain_values = []
         with open(self.file) as file:
-            for line in file:
+            for count, line in enumerate(file):
                 plain_values.clear()
                 for index, value in enumerate(line.split(",")):
                     plain_values.append(value.rstrip())
@@ -76,8 +82,16 @@ class file_parser:
                 if len(plain_values)-1 < self.y_column:
                     raise ValueError("LINE IN FILE IS SHORTER THAN %n" % (self.y_column))
 
-                self.x_data.append(float(plain_values[self.x_column]))
-                self.y_data.append(float(plain_values[self.y_column]))
+                if self.x_column>=0:
+                    self.x_data.append(float(plain_values[self.x_column]))
+                else:
+                    self.x_data.append(count)
+
+                if self.y_column>=0:
+                    self.y_data.append(float(plain_values[self.y_column]))
+                else:
+                    self.y_data.append(count)
+
                 xerr = 0
                 for err_number, x_err_type in enumerate(self.x_err_types):#TODO: USE GET_ERROR?
                     column_count = 0
@@ -90,6 +104,8 @@ class file_parser:
                         xerr += uncert_lib.percent_uncert(float(plain_values[self.x_column]),self.x_err_setting[err_number])
                     if(x_err_type == "plain"):
                         xerr += float(self.x_err_setting[err_number])
+                    if(x_err_type == "add"):
+                        xerr += float(plain_values[self.x_settings_columns[column_count]])
                 self.x_err.append(xerr)
                 yerr = 0
                 for err_number, y_err_type in enumerate(self.y_err_types):#TODO: USE GET_ERROR?
@@ -103,6 +119,8 @@ class file_parser:
                         yerr += uncert_lib.percent_uncert(float(plain_values[self.y_column]),self.y_err_setting[err_number])
                     if(y_err_type == "plain"):
                         yerr += float(self.y_err_setting[err_number])
+                    if(y_err_type == "add"):
+                        yerr += float(plain_values[self.y_settings_columns[column_count]])
                 self.y_err.append(yerr)
 
     def redefine_x(self,filename,column,formular,propagation,uncert_types=[],uncert_settings=[],uncert_settings_columns=[]):
@@ -124,12 +142,21 @@ class file_parser:
             for i,x in enumerate(self.x_data):
                 self.x_data[i] = formular(x,float(calc_values[0]))
         else:
-            raise ValueError("SIZE OF DATASET SHOULD BE: %n OR 1 BUT IS: %n" % (len(self.x_data),len(calc_values)))
+            raise ValueError("SIZE OF DATASET SHOULD BE: %d OR 1 BUT IS: %d" % (len(self.x_data),len(calc_values)))
 
-    def shift_x(self,value):
+    def offset_x(self,value):#TODO: ALL IN MODIFY
+        for i,x in enumerate(self.x_data):
+            self.x_data[i] = x+value
+
+    def stretch_x(self,value):
         for i,x in enumerate(self.x_data):
             self.x_data[i] = x*value
             self.x_err[i] = self.x_err[i]*value
+
+    def modify_x(self,formular,propagation):
+        for i,x in enumerate(self.x_data):
+            self.x_data[i] = formular(x)
+            self.x_err[i] = propagation(x,self.x_err[i])
 
     def redefine_y(self,filename,column,formular,propagation,uncert_types=[],uncert_settings=[],uncert_settings_columns=[]):
         calc_values = []
@@ -153,10 +180,19 @@ class file_parser:
         else:
             raise ValueError("SIZE OF DATASET SHOULD BE: %n OR 1 BUT IS: %n" % (len(self.y_data),len(calc_values)))
 
-    def shift_y(self,value):
+    def offset_y(self,value):
+        for i,y in enumerate(self.y_data):
+            self.y_data[i] = y+value
+
+    def stretch_y(self,value):
         for i,y in enumerate(self.y_data):
             self.y_data[i] = y*value
             self.y_err[i] = self.y_err[i]*value
+
+    def modify_y(self,formular,propagation):
+        for i,y in enumerate(self.y_data):
+            self.y_data[i] = formular(y)
+            self.y_err[i] = propagation(y,self.y_err[i])
 
     def check_cohearance(self):
         if not (isinstance(self.file, str) and isinstance(self.x_column, int) and isinstance(self.y_column, int) and isinstance(self.x_settings_columns, list) and isinstance(self.y_settings_columns, list)
@@ -166,17 +202,23 @@ class file_parser:
         if(not os.path.exists(self.file)):
             raise ValueError("FILE: %s NOT FOUND!" % (self.file))
 
-        if(self.x_column < 0):
+        if(self.x_column < -1):
             raise ValueError("X COLUMN INVALID!")
 
-        if(self.y_column < 0):
+        if(self.y_column < -1):
             raise ValueError("Y COLUMN INVALID!")
 
     def get_data(self):
         return [self.x_data,self.y_data]
     def get_uncert(self):
         return [self.x_err,self.y_err]
-
+    def get_fit_params(self):
+        return self.fit_params
+    def get_fit_params_uncerts(self):
+        return self.fit_params_uncert
+    def set_fit_params_and_uncerts(self,param,uncert):
+        self.fit_params.append(param)
+        self.fit_params_uncert.append(uncert)
     #self.file = ""
     #self.x_column = 0
     #self.y_column = 0
@@ -213,7 +255,13 @@ class ez_parser:
         prepared_uncert = prepared_uncert*Decimal(str(additional))
         prepared_value = prepared_value*Decimal(str(additional))
         prepared_uncert = np.ceil(prepared_uncert)
-        prepared_value = round(prepared_value,0)
+        #prepared_value = round(prepared_value,0)
+
+        try: #TODO: CONSIDER
+            prepared_value = round(prepared_value,0)
+        except DecimalException as e:
+            prepared_value = prepared_value
+
         prepared_uncert = prepared_uncert/additional
         prepared_value = prepared_value/additional
 
@@ -236,8 +284,14 @@ class ez_parser:
             prepared_uncert = int(prepared_uncert)
             uncert_is_int = True
         #stringify
-        str_value = str('{:f}'.format(prepared_value))
-        str_uncert = str('{:f}'.format(prepared_uncert))
+        if value_is_int:
+            str_value = str(prepared_value)
+        else:
+            str_value = str('{:f}'.format(prepared_value))
+        if uncert_is_int:
+            str_uncert = str(prepared_uncert)
+        else:
+            str_uncert = str('{:f}'.format(prepared_uncert))
         
         if not uncert_is_int:
             decimals_uncert = (len(str_uncert)-1) - str_uncert.find(".")
@@ -252,9 +306,6 @@ class ez_parser:
                     str_value += "."
                 for x in range(decimals_uncert - decimals_value):
                     str_value += "0"
-        
-
-        #print(str(value) + " +/- " + str(uncert) + " -> " + str(str_value) + " +/- " + str(str_uncert))
         return [str_value,str_uncert]
 
     def describe(self, x_label="", y_label=""):
@@ -275,14 +326,27 @@ class ez_parser:
     def redefine_x(self,filename,filename_redefine,column,formular,propagation,uncert_types=[],uncert_settings=[],uncert_settings_columns=[]):
         self.file_parsers[filename].redefine_x(filename_redefine,column,formular,propagation,uncert_types,uncert_settings,uncert_settings_columns)
     
-    def shift_x(self,filename,value):
-        self.file_parsers[filename].shift_x(value)
+    def offset_x(self,filename,value):
+        self.file_parsers[filename].offset_x(value)
+
+    def stretch_x(self,filename,value):
+        self.file_parsers[filename].stretch_x(value)
+
+    def modify_x(self,filename,formular,propagation):
+        self.file_parsers[filename].modify_x(formular,propagation)
 
     def redefine_y(self,filename,filename_redefine,column,formular,propagation,uncert_types=[],uncert_settings=[],uncert_settings_columns=[]):
         self.file_parsers[filename].redefine_y(filename_redefine,column,formular,propagation,uncert_types,uncert_settings,uncert_settings_columns)
 
-    def shift_y(self,filename,value):
-        self.file_parsers[filename].shift_y(value)
+    def offset_y(self,filename,value):
+        self.file_parsers[filename].offset_y(value)
+        
+
+    def stretch_y(self,filename,value):
+        self.file_parsers[filename].stretch_y(value)
+
+    def modify_y(self,filename,formular,propagation):
+        self.file_parsers[filename].modify_y(formular,propagation)
 
     def setup_file(self, filename,x_column,y_column,x_settings_columns=[],y_settings_columns=[],x_err_types=[],y_err_types=[],x_err_setting=[],y_err_setting=[]):
         self.file_parsers[filename] = file_parser(filename,x_column,y_column,x_settings_columns,y_settings_columns,x_err_types,y_err_types,x_err_setting,y_err_setting)
@@ -314,7 +378,7 @@ class ez_parser:
             plt.xlabel(x_label)
 
         if(len(y_label)>0):
-            plt.ylabel(y_label,rotation='horizontal', ha='right')
+            plt.ylabel(y_label)#,rotation='horizontal', ha='right')
 
         if(len(additional_legend)>0):
             plt.plot([], [], ls='none', label=additional_legend)
@@ -326,7 +390,7 @@ class ez_parser:
         if(show):
             plt.show()
 
-    def fit_plot(self, filenames, models, parameter_counts, colors=[], title="", x_label="", y_label="", legends_err=[], legends_fit=[], additional_legend="", override_start=[], show=True):
+    def fit_plot(self, filenames, models, parameter_counts, colors=[], title="", x_label="", y_label="", legends_err=[], legends_fit=[], additional_legend="", override_start=[], param_names=[], show=True, debug=False):
         if not isinstance(filenames, list):
             filenames = [filenames]
 
@@ -336,8 +400,8 @@ class ez_parser:
         if not isinstance(parameter_counts, list):
             parameter_counts = [parameter_counts]
 
-        if len(legends_err)<=0:
-            for count in range(len(filenames)):
+        if len(legends_err)<=len(filenames):
+            for count in range(len(filenames) - len(legends_err)):
                 legends_err.append("")
 
         if not isinstance(legends_err, list):
@@ -350,7 +414,7 @@ class ez_parser:
         if not isinstance(legends_fit, list):
             legends_fit = [legends_fit]
 
-        if len(override_start)<=0:
+        if len(override_start)<=0:#TODO: fix no param
             for count in range(len(filenames)):
                 temp = []
                 for parameters in range(parameter_counts[count]):
@@ -367,16 +431,21 @@ class ez_parser:
                 plt.errorbar(x,y,
                                 xerr=xerr,yerr=yerr, label=legends_err[parser_number],
                                 marker='x', linestyle="none", markersize=2, markeredgewidth=0.5, capsize=3, linewidth=0.5, color=colors[parser_number])
-                regression = perform_odr(models[parser_number], parameter_counts[parser_number], x, y, xerr, yerr, override_start[parser_number])
+                regression = perform_odr(models[parser_number], parameter_counts[parser_number], x, y, xerr, yerr, override_start[parser_number],debug)
                 plt.plot(x_line, models[parser_number](regression.beta,x_line), alpha=0.25, linewidth="1.5", label=legends_fit[parser_number],color=colors[parser_number])#TODO: COLORS OBLIGATORY
                 self.fit_values = regression.beta
                 self.fit_uncerts = regression.sd_beta
                 label = ""
                 for parameter_number, beta in enumerate(regression.beta):
+                    if len(param_names)>0:#TODO: support different models
+                        curr_param = "{" + param_names[parameter_number] + "}"
+                    else:
+                        curr_param = ascii_lowercase[parameter_number]
+                    self.file_parsers[filename].set_fit_params_and_uncerts(beta,regression.sd_beta[parameter_number])
                     x, xerr = self.reduce(beta,regression.sd_beta[parameter_number])
-                    label += "\n$" + ascii_lowercase[parameter_number] + " \\ = \\ " + x + "$" 
-                    label += "\n$u_" + ascii_lowercase[parameter_number] + " \\ = \\ " + xerr + "$" 
-                label += "\n$\\frac{\\chi^2}{dof} \\ = \\ " + str(regression.res_var) + "$"
+                    label += "\n$" + curr_param + " \\ = \\ " + x + "$" 
+                    label += "\n$u_" + curr_param + " \\ = \\ " + xerr + "$" 
+                label += "\n$\\frac{\\chi^2}{dof} \\ " + u"\u2248" + " \\ " + str(round_sig(regression.res_var)) + "$"
                 plt.plot([], [], ' ', label=label)
             else:
                 raise ValueError("NO INITIALIZED PARSER FOR FILE: %s" % (filename))
@@ -388,7 +457,7 @@ class ez_parser:
             plt.xlabel(x_label)
 
         if(len(y_label)>0):
-            plt.ylabel(y_label, rotation='horizontal', ha='right')
+            plt.ylabel(y_label)#, rotation='horizontal', ha='right')
 
         if(len(additional_legend)>0):
             plt.plot([], [], ls='none', label=additional_legend)
